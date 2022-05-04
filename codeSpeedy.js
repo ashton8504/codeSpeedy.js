e1b9b790b10572bcfd0cf8a7950b7a568cb7e220
Ashton Little <ashtonlittle@Ashtons-iMac.columbus.rr.com>
2022-05-04 08:51:38 -0400

    solution 2

--- codeSpeedy.js
+++ codeSpeedy.js
@@ -10,8 +10,16 @@
 
 //String.prototype.charAt()
 //This is setting a string to hello world
-str = 'Hello World';
+// str = 'Hello World';
 //charAt() is a string object method(function) that takes an index as an argument
 //We are saying (6) and since its 0 based index the answer is "w"
-character = str.charAt(6);
-console.log(character)
+// character = str.charAt(6);
+// console.log(character)
+
+//Building charAt() from scratch
+//this is variable and we are setting a string to hello world
+str = 'Hello World';
+//This is saying index is equal to position 6 on a 0 based index
+index = 6;
+//this is console logging str and putting index into an array to find the solution which is still W
+console.log(str[index])
