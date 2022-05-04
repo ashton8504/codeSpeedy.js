28fc22f1086e8f2a8b256724c425f851430e0850
Ashton Little <ashtonlittle@Ashtons-iMac.columbus.rr.com>
2022-05-04 09:20:41 -0400

    submitting more code

--- concat.js
+++ concat.js
@@ -13,12 +13,21 @@ console.log(str1.concat(', ', 'super ', str2))
 
 //Building concat() from scratch
 
+//Setting up a function named concat
+//concat returns two or more joined strings
+//A spread allows us to iterate as an array expression or string to be expanded
+//If we only used str and not the spread results would just be Hello
 function concat(...str) {
+    //setting up an empty string named concat_str
     concat_str = ''
+    //for loop and we are looking for i in str
     for (i in str) {
+        //this is setting up concat_str to add or be equal to str[i]
         concat_str += str[i]
     }
+    //this will return the result of concat_str
     return concat_str
 }
+//console logs to print to results console
 console.log(concat('Hello', ', ', 'World'))
 console.log(concat('Hello', ', ', 'super ', 'World'))
