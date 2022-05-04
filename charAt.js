a414ba5bf7b69d88da8e5bf6753198f953f72f9c
Ashton Little <ashtonlittle@Ashtons-iMac.columbus.rr.com>
2022-05-04 09:05:49 -0400

    updating project

--- charAt.js
+++ charAt.js
@@ -24,15 +24,3 @@ index = 6;
 //this is console logging str and putting index into an array to find the solution which is still W
 console.log(str[index])
 
-//String.prototype.concat()
-//concat() method (function) concatenates (combines) the string we call to the arguments passed to it as a string
-//This is setting string one to Hello
-str1 = 'Hello'
-//Setting string two to World
-str2 = 'World'
-//here we are combining str1 to concat with two parameters
-//Result is Hello, World
-console.log(str1.concat(', ', str2))
-//Here we are combining str1 to contact and using 3 parameters when we log
-//Result = Hello, Super World
-console.log(str1.concat(', ', 'super ', str2))
